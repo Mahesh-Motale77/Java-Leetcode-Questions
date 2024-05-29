66bc83f16dbeab66789e08f70a2a45ee52f08fe9
Mahesh Motale <155712006+Mahesh-Motale77@users.noreply.github.com>
2024-05-29 14:06:34 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Number: ");
+        System.out.println("Enter the Number to print fibonacci series: ");
         int n = sc.nextInt();
 
         int x = 0;
